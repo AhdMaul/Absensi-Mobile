b117551d16ac817a1769ac11dca724fc36ae2419
AhdMaul <a.maulana.cnplus@gmail.com>
2025-10-27 10:16:58 +0700

    last one

--- eshrm_web/frontend/src/pages/Login.jsx
+++ eshrm_web/frontend/src/pages/Login.jsx
@@ -35,7 +35,7 @@ export default function Login() {
                             type="email"
                             placeholder="contoh@email.com"
                             value={email}
-                            onChange={(e) => setEmail(e.target.value)}
+                            onChange={(e) => setEmail(e.tavrget.value)}
                             required
                             className="w-full px-4 py-2 sm:py-2.5 border border-gray-300 dark:border-gray-600 rounded-lg bg-white dark:bg-gray-700 text-gray-800 dark:text-gray-100 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition"
                         />
